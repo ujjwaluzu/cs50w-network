2c6a38d039225bb2834dd8d97ede36e1401092f1
Ujjwal Baunthiyal <baunthiyalujjwal52@gmail.com>
2026-05-21 17:07:40 +0530

    followers/following model added

--- network/models.py
+++ network/models.py
@@ -3,13 +3,14 @@ from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    following = models.ManyToManyField("self", symmetrical=False)
 
 
 # post model
 
 class Post(models.Model):
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
+    title = models.CharField(max_length=50)
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
-    likes = models.ManyToManyField(User)
+    likes = models.ManyToManyField(User, related_name="liked_posts")
