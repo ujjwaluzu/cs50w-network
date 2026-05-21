0d98cdd90c0c740288bf30a3d2994ba95f137ccf
Ujjwal Baunthiyal <baunthiyalujjwal52@gmail.com>
2026-05-21 16:26:23 +0530

    Post model created

--- network/models.py
+++ network/models.py
@@ -4,3 +4,12 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+
+# post model
+
+class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    likes = models.ManyToManyField(User)
